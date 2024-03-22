6e6222971eabce4bc9fd9d12c2d00c87672f953b
XavielCesarJoao <xavielcesar.joao@gmail.com>
2024-03-22 23:27:23 +0100

    HEY-12: adding global funcion file

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\User;
+use App\Models\{Question, User};
 
 use function Pest\Laravel\{actingAs, assertDatabaseHas, post, put};
 
@@ -8,7 +8,7 @@ it('should be able to vote like a question', closure: function () {
 
     // Arrange
     $user     = User::factory()->create();
-    $question = \App\Models\Question::factory()->create();
+    $question = Question::factory()->create();
     actingAs($user);
     // Act
     post(route('question.like', $question))->assertRedirect();
@@ -20,4 +20,20 @@ it('should be able to vote like a question', closure: function () {
         'user_id'     => $user->id,
     ]);
 
+});
+
+it('shoul not be able to like more than 1 time', function () {
+    // Arrange
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+    actingAs($user);
+
+    // Act
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    // Assert
+    expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
 });
