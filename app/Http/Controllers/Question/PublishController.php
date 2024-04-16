c14fe157fd67de8a612a7146d933e2c4d37bd02f
XavielCesarJoao <134645093+XavielCesarJoao@users.noreply.github.com>
2024-04-16 16:42:51 +0100

    HEY-15: should be able to publish a question

---
+++ app/Http/Controllers/Question/PublishController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Question;
+
+use App\Http\Controllers\Controller;
+use App\Models\Question;
+use Illuminate\Http\{RedirectResponse};
+
+class PublishController extends Controller
+{
+    //
+    public function __invoke(Question $question): RedirectResponse
+    {
+
+        // TODO: Implement __invoke() method.
+        $question->update(['draft' => false]);
+
+        return back();
+    }
+}
